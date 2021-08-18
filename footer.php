7d2ef561998759c1d28b9ad86b72999d00fdbd84
dduyen <10082000duyen@gmail.com>
2021-08-18 13:12:08 +0700

    frontend

---
+++ footer.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<<div id="footer">
+        <ul style="list-style-type:none" class="d-flex justify-content-center">
+            <li ><a href="#">ホーム</a></li>
+            <li><a href="#">会社概要</a></li>
+            <li><a href="#">お問い合わせ</a></li>
+            <li><a href="#">エントリー</a></li>
+        </ul>
+
+        <h2 class="d-flex justify-content-center"><img src="./imgs/i_footer_logo (1).png" alt="" width="230" height="70" ></h2>
+        <p class="d-flex justify-content-center text-light" style=" margin-bottom:2%;">	&copy; Upraise,Inc. All Rights Reserved.</p>
+    </div>
+    <div id="btnscroll" class="fixed-bottom"> <i  class="fa fa-arrow-up" aria-hidden="true"></i></div>
+</body>
+</html>
---
+++ header.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.0/css/all.css" integrity="sha384-lZN37f5QGtY3VHgisS14W3ExzMWZxybE1SJSEsQp9S+oqd12jhcu+A56Ebc1zFSJ" crossorigin="anonymous">
+    <link rel="stylesheet" href="./css/home.css">
+</head>
+<body>
+<div id="top">
+        <div class="container">
+            <div class="row ">
+                <div class="col-md-10 col-sm-12 border-bottom">
+                <i class="fa fa-facebook-square" aria-hidden="true"></i> 日本留学安心情報サイト「アプライズ」は、アジアの将来を担う人材の創出・育成を通じて産業発展に貢献します。
+                </div>
+                <div class="col-md-2 border-bottom">
+                    <a href="https://www.facebook.com/upraise.Ltd/" class="text-success">FACEBOOK 
+                     <i class="fab fa-facebook-square "></i></a>
+                </div>
+            </div>
+        </div> 
+        <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-Piv4xVNRyMGpqkS2by6br4gNJ7DXjqk09RmUpJ8jgGtD7zP9yug3goQfGII0yAns" crossorigin="anonymous"></script>       
+</body>
+</html>
